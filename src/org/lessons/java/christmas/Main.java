b1b13d007bb197f558dae6c416a2a65f850e7aea
Mark-dev30 <fronterremarco@gmail.com>
2023-05-12 11:53:48 +0200

    first commit

---
+++ src/org/lessons/java/christmas/Main.java
@@ -0,0 +1,47 @@
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		List<String> myList = new ArrayList<>();
+		boolean boll = true;
+		Scanner in = new Scanner(System.in);
+		//FIRST METHOD
+//		while(boll) {
+//			System.out.println("Inserisci un desiderio o scrivi 'annulla' per fermare l'esecuzione");
+//			String wish = in.nextLine();
+//			if(wish.equals("annulla")) {
+//				boll = false;
+//				System.out.println("-------Lista dei desideri: -------");
+//				for(String val : myList) {
+//					System.out.println(val);
+//				}
+//			}
+//			else {
+//				myList.add(wish);
+//				System.out.println("La lunghezza della lista è: " + myList.size());
+//			}
+//		}
+		//SECOND METHOD
+		while(boll) {
+			System.out.println("Inserisci un desiderio");
+			String wish = in.nextLine();
+			myList.add(wish);
+			System.out.println("La lunghezza della lista è: " + myList.size());
+			System.out.println("Se Vuoi continuare a inserire un desiderio scrivi 'si'?");
+			String request = in.nextLine();
+			if(!request.equals("si")) {
+				boll = false;
+				System.out.println("-------Lista dei desideri: -------");
+				for(String val : myList) {
+					System.out.println(val);
+				}
+			}
+		}
+		
+		
+	}
+}
